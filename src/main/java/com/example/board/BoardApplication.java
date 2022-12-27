544e01a67ca8fad7c805928a2d2ae90e66f8f691
JuyoungKoo <juyoungkoo9212@gmail.com>
2022-12-27 16:35:31 +0900

    war 설정 추가

--- src/main/java/com/example/board/BoardApplication.java
+++ src/main/java/com/example/board/BoardApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BoardApplication {
+public class BoardApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(BoardApplication.class, args);
 	}
-
+	
+	
 }
